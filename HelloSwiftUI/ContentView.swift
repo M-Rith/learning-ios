6479000d64d279b11117f44c06ea0978970e3b39
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-08 00:12:27 +0700

    first commit

---
+++ HelloSwiftUI/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by AppleD0g on 3/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+        Text("hi")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by AppleD0g on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
