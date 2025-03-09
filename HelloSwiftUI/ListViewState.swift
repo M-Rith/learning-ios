67682c203c0bda9f46b0f2c138bf45ba5f5abd8c
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-09 20:43:27 +0700

    fixing

--- HelloSwiftUI/ListViewState.swift
+++ HelloSwiftUI/ListViewState.swift
@@ -10,27 +10,60 @@ import SwiftUI
 struct ListViewState: View {
     
     @State private var name : String = ""
-    @State private var friends : [String] = []
+//    @State private var friends : [String] = []
+    
+    @State private var searchName: String = ""
+    
+    
+    @State private var friends: [String] = ["John", "Mary", "Steven", "Steve", "Jerry"]
+    @State private var filterFriends: [String] = []
     
     var body: some View {
+//        VStack {
+//            TextField("Enter your name" , text: $name)
+//                .textFieldStyle(.roundedBorder)
+//                .onSubmit {
+//                    friends.append(name)
+//                    name=""
+//                }
+//            
+//            List(friends, id: \.self) { friend in
+//                Text(friend)
+//            }
+//            
+//            
+//            Spacer()
+//        }.padding()
+        
+        
+        
         VStack {
-            TextField("Enter your name" , text: $name)
-                .textFieldStyle(.roundedBorder)
-                .onSubmit {
-                    friends.append(name)
-                    name=""
-                }
-            
-            List(friends, id: \.self) { friend in
+            List(filterFriends, id: \.self) { friend in
                 Text(friend)
             }
+            .listStyle(.plain)
+            .searchable(text: $searchName)
+            .onChange(of: searchName) {
             
-            
+                
+                if searchName.isEmpty {
+                    filterFriends = friends
+                }
+                else {
+                    filterFriends = friends.filter { $0.contains(searchName)}
+                }
+            }
             Spacer()
         }.padding()
+        .onAppear(perform: {
+            filterFriends = friends
+        })
+        .navigationTitle("Friends")
     }
 }
 
 #Preview {
-    ListViewState()
+    NavigationStack {
+        ListViewState()
+    }
 }
