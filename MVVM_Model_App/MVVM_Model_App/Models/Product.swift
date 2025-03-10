df27651f8b26bde02fcb6462ebc4d51082f8b9d2
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-10 23:56:06 +0700

    finish MVVM partern app

---
+++ MVVM_Model_App/MVVM_Model_App/Models/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  MVVM_Model_App
+//
+//  Created by AppleD0g on 3/10/25.
+//
+
+
+import Foundation
+
+struct Product: Decodable {
+    let id: Int
+    let title: String
+    let price: Double
+}
