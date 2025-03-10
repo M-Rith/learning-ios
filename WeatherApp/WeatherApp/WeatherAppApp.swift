cce47637c8feb40943a0b54727154ce98b9d826d
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-10 11:02:56 +0700

    init-weather-app

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by AppleD0g on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
