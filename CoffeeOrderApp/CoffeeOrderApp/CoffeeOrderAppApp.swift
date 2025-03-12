f44a776b76321de47d9fda3d9880bfdd100a60aa
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-12 12:53:05 +0700

    init coffee order

---
+++ CoffeeOrderApp/CoffeeOrderApp/CoffeeOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderAppApp.swift
+//  CoffeeOrderApp
+//
+//  Created by AppleD0g on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
