639c5e0de57da1932e231b50b8062807ac436d3e
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-12 15:57:07 +0700

    start-to-working

---
+++ CoffeeOrderApp/CoffeeOrderApp/ViewModels/CoffeeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  CoffeeViewModel.swift
+//  CoffeeOrderApp
+//
+//  Created by AppleD0g on 3/12/25.
+//
+
+import Foundation
+import Observation
+
+
+@Observable class CoffeeViewModel {
+    private let webService = WebService()
+    
+    var orders: [Order] = []
+    
+    func getOrders() async {
+        self.orders = (try? await webService.getOrders()) ?? []
+    }
+}
---
+++ CoffeeOrderApp/CoffeeOrderApp/Views/OrderCellView.swift
@@ -0,0 +1,25 @@
+//
+//  OrderCellView.swift
+//  CoffeeOrderApp
+//
+//  Created by AppleD0g on 3/12/25.
+//
+
+import SwiftUI
+
+struct OrderCellView: View {
+    let order: Order
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(order.name).accessibilityIdentifier("OrderNameText").bold()
+                
+                Text("\(order.cofeeName) (\(order.size.rawValue))").accessibilityIdentifier("CoffeeNameAndSizeText").opacity(0.5)
+            }
+            Spacer()
+            Text(order.total, format: .currency(code: "USD")) // ✅ SwiftUI
+                .accessibilityIdentifier("CoffeePriceText")
+        }
+    }
+}
